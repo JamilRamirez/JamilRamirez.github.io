2def41f013e34e94c7bf9d321face4920a93375a
Jamil Ramirez <jamil_ramirez.m@hotmail.com>
2026-07-04 20:30:15 -0500

    Website update

--- update_site - copia.bat
+++ update_site - copia.bat
@@ -1,7 +1,9 @@
+@echo off
+cd /d E:\JAMIL\Blog\jamil-blog
 git pull --rebase origin main
 quarto render
 git add .
+git status
 git commit -m "Website update"
 git push
-quarto publish gh-pages
 pause
--- update_site.bat
+++
@@ -1,6 +0,0 @@
-quarto render
-git add .
-git commit -m "Website update"
-git push
-quarto publish gh-pages
-pause
